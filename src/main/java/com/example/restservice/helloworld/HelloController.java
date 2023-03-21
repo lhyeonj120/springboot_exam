7856541ae6a7b532fd62c62b742045f7f6975876
lhyeonj120 <isk04387@gmail.com>
2023-03-21 12:04:43 +0900

    add Path Variable

--- src/main/java/com/example/restservice/helloworld/HelloController.java
+++ src/main/java/com/example/restservice/helloworld/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.restservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World Bean!!!");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean2(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World Bean, %s", name));
     }
 }

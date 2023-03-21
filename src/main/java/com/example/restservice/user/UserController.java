b492c1f027c4d18f0639cd8b6d2932843c58a591
lhyeonj120 <isk04387@gmail.com>
2023-03-21 12:32:00 +0900

    feat: add retrieveOneUser

--- src/main/java/com/example/restservice/user/UserController.java
+++ src/main/java/com/example/restservice/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.restservice.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +17,10 @@ public class UserController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveOneUser(@PathVariable int id){
+        return service.findOne(id);
     }
 }

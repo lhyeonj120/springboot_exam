f7917111850d6faab2ddf5d47f43a58133beaee9
lhyeonj120 <isk04387@gmail.com>
2023-03-21 11:42:35 +0900

    add hello-world-bean

--- src/main/java/com/example/restservice/helloworld/HelloController.java
+++ src/main/java/com/example/restservice/helloworld/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String helloWorld(){
         return "Hello World!!!";
     }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World Bean!!!");
+    }
 }

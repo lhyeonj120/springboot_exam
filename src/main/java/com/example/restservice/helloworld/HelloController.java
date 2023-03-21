9c13bfbdc3c2ca8213895f53ec55ef58596c80b7
lhyeonj120 <isk04387@gmail.com>
2023-03-21 16:26:20 +0900

    add multilingual processing

--- src/main/java/com/example/restservice/helloworld/HelloController.java
+++ src/main/java/com/example/restservice/helloworld/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.restservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -20,5 +27,12 @@ public class HelloController {
     @GetMapping("/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean2(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World Bean, %s", name));
+    }
+
+    @GetMapping("/hello-world-international")
+    public String helloWorldInternational(
+            //언어에 대한 정보 전달 필요, 기본 - 한국어
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }

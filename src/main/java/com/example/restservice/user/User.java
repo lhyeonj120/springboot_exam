cf9919d9ea4f7a0d9f94e8ea0b0e4c0ce6bf86ef
lhyeonj120 <isk04387@gmail.com>
2023-03-22 12:47:45 +0900

    add post, user relation

--- src/main/java/com/example/restservice/user/User.java
+++ src/main/java/com/example/restservice/user/User.java
@@ -10,8 +10,10 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -30,4 +32,7 @@ public class User {
     //@JsonIgnore
     private String password;
     private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 }

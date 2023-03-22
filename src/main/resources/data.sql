6fb7aea2fd797d2aa6c34ca81203ab7dcdd0972b
lhyeonj120 <isk04387@gmail.com>
2023-03-22 10:49:11 +0900

    add insert data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'User1', 'test1', '70000');
+insert into user values(2, sysdate(), 'User2', 'test2', '70000');
+insert into user values(3, sysdate(), 'User3', 'test3', '70000');
